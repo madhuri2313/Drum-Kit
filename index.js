81a64b8b83e52b790491ee7de3270a638606a0de
madhuri2313 <madhurisharma.se@gmail.com>
2023-06-13 05:48:20 +0530

    second-commit

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 
 var buttonPosition = document.querySelectorAll(".drum").length;
 
+var audio = new Audio("sounds/tom-2.mp3")
+
 for (var m = 0; m<buttonPosition; m++) {
     document.querySelectorAll(".drum")[m].addEventListener("click", function() {
-        alert("i got clicked")
+        var sound = new Audio('sounds/tom-2.mp3');
+
+        audio.play();
     })
 }
